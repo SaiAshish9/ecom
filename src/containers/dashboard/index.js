328b04a62f875a38ef59f861185a2db4c34bdf20
SaiAshish9 <saiashish3760@gmail.com>
2020-06-11 22:42:06 +0530

    applied prittier

--- src/containers/dashboard/index.js
+++ src/containers/dashboard/index.js
@@ -1,304 +1,248 @@
-import React from 'react'
-import Navbar from '../../components/dashboard/navbar'
-import Grid from '@material-ui/core/Grid'
-import withStyles from '@material-ui/core/styles/withStyles'
-import Paper from '@material-ui/core/Paper'
-import IconButton from '@material-ui/core/IconButton'
+import React from "react";
+import Navbar from "../../components/dashboard/navbar";
+import Grid from "@material-ui/core/Grid";
+import withStyles from "@material-ui/core/styles/withStyles";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
 
-import {IoIosSpeedometer,IoIosDocument} from 'react-icons/io'
-import LoyaltyIcon from '@material-ui/icons/Loyalty';
-import {FaBoxOpen,FaUserCircle,FaStore,FaUsers,FaAppStoreIos} from 'react-icons/fa'
-import {AiFillAppstore} from 'react-icons/ai'
-import LaptopMacIcon from '@material-ui/icons/LaptopMac';
-import {FiSettings} from 'react-icons/fi'
-import {RiLogoutBoxRLine} from 'react-icons/ri'
+import { IoIosSpeedometer, IoIosDocument } from "react-icons/io";
+import LoyaltyIcon from "@material-ui/icons/Loyalty";
+import {
+  FaBoxOpen,
+  FaUserCircle,
+  FaStore,
+  FaUsers,
+  FaAppStoreIos,
+} from "react-icons/fa";
+import { AiFillAppstore } from "react-icons/ai";
+import LaptopMacIcon from "@material-ui/icons/LaptopMac";
+import { FiSettings } from "react-icons/fi";
+import { RiLogoutBoxRLine } from "react-icons/ri";
 
-import {withRouter,Switch,Route} from 'react-router-dom'
+import { withRouter, Switch, Route } from "react-router-dom";
 
-import Store from '../../components/dashboard/store'
-import Settings from '../../components/dashboard/settings'
-import Products from '../../components/dashboard/products'
-import Categories from '../../components/dashboard/categories'
-import Options from '../../components/dashboard/categories/categoryoptions'
+import Store from "../../components/dashboard/store";
+import Settings from "../../components/dashboard/settings";
+import Products from "../../components/dashboard/products";
+import Categories from "../../components/dashboard/categories";
+import Options from "../../components/dashboard/categories/categoryoptions";
 
-
-
-
-const Dashboard = ({classes,history}) => {
-
-
-
-
-const icons=[
-   
+const Dashboard = ({ classes, history }) => {
+  const icons = [
     {
-        icon:<IoIosSpeedometer/>,
-        paths:[
-            '/dashboard'
-        ]
-        },
-    {
-        icon:<AiFillAppstore/>,
-        paths:[
-            '/dashboard/orders'
-        ],
-        name:'orders'
+      icon: <IoIosSpeedometer />,
+      paths: ["/dashboard"],
     },
     {
-        icon:<LoyaltyIcon/>,
-        paths:[
-            '/dashboard/product',
-            '/dashboard/product/products/edit',
-            '/dashboard/product/products/edit/options',
-            '/dashboard/product/products/edit/options/edit',
-            '/dashboard/product/categories',
-            '/dashboard/product/options'
-
-        ],
-        name:'product'
+      icon: <AiFillAppstore />,
+      paths: ["/dashboard/orders"],
+      name: "orders",
     },
     {
-        icon:<FaBoxOpen/>,
-        paths:[
-            '/dashboard/categories',
-            '/dashboard/categories/edit',
-            '/dashboard/categories/options'
-        ],
-        name:'categories'
+      icon: <LoyaltyIcon />,
+      paths: [
+        "/dashboard/product",
+        "/dashboard/product/products/edit",
+        "/dashboard/product/products/edit/options",
+        "/dashboard/product/products/edit/options/edit",
+        "/dashboard/product/categories",
+        "/dashboard/product/options",
+      ],
+      name: "product",
     },
     {
-        icon:<FaUsers/>,
-        paths:[
-            '/dashboard/customers'
-        ],
-        name:'customers'
+      icon: <FaBoxOpen />,
+      paths: [
+        "/dashboard/categories",
+        "/dashboard/categories/edit",
+        "/dashboard/categories/options",
+      ],
+      name: "categories",
     },
     {
-        icon:<IoIosDocument/>,
-        paths:[
-            '/dashboard/coupons'
-        ],
-        name:'coupons'
+      icon: <FaUsers />,
+      paths: ["/dashboard/customers"],
+      name: "customers",
     },
     {
-        icon:<FaAppStoreIos/>,
-        paths:[
-            '/dashboard/apps'
-        ],
-        name:'apps'
+      icon: <IoIosDocument />,
+      paths: ["/dashboard/coupons"],
+      name: "coupons",
     },
     {
-        icon:<LaptopMacIcon/>,
-        paths:[
-            '/dashboard/services'
-        ],
-        name:'services'
+      icon: <FaAppStoreIos />,
+      paths: ["/dashboard/apps"],
+      name: "apps",
     },
     {
-        icon:<FaStore/>,
-        paths:[
-            '/dashboard/store',
-            '/dashboard/store/theme',
-            '/dashboard/store/domain',
-            '/dashboard/store/shipping',
-            '/dashboard/store/locations',
-            '/dashboard/store/locations/edit',
-            '/dashboard/store/payment',
-            '/dashboard/store/payment/edit',
-            '/dashboard/store/products',
-            '/dashboard/store/domain/edit'
-        ],
-        name:'store'
+      icon: <LaptopMacIcon />,
+      paths: ["/dashboard/services"],
+      name: "services",
     },
     {
-        icon:<FaUserCircle/>,
-        paths:[
-            '/dashboard/users'
-        ],
-        name:'users'
+      icon: <FaStore />,
+      paths: [
+        "/dashboard/store",
+        "/dashboard/store/theme",
+        "/dashboard/store/domain",
+        "/dashboard/store/shipping",
+        "/dashboard/store/locations",
+        "/dashboard/store/locations/edit",
+        "/dashboard/store/payment",
+        "/dashboard/store/payment/edit",
+        "/dashboard/store/products",
+        "/dashboard/store/domain/edit",
+      ],
+      name: "store",
     },
     {
-        icon:<FiSettings/>,
-        paths:[
-            '/dashboard/settings',
-            '/dashboard/settings/details/edit',
-            '/dashboard/settings/Subscription',
-            '/dashboard/settings/preferences',
-            '/dashboard/settings/balance',
-            '/dashboard/settings/contact',
-            '/dashboard/settings/language',
-            '/dashboard/settings/privacy',
-            '/dashboard/settings/terms'
-
-
-
-
-        ],
-        name:'settings'
+      icon: <FaUserCircle />,
+      paths: ["/dashboard/users"],
+      name: "users",
     },
     {
-        icon:<RiLogoutBoxRLine/>,
-        paths:[
-            '/'
-        ]
-    }
+      icon: <FiSettings />,
+      paths: [
+        "/dashboard/settings",
+        "/dashboard/settings/details/edit",
+        "/dashboard/settings/Subscription",
+        "/dashboard/settings/preferences",
+        "/dashboard/settings/balance",
+        "/dashboard/settings/contact",
+        "/dashboard/settings/language",
+        "/dashboard/settings/privacy",
+        "/dashboard/settings/terms",
+      ],
+      name: "settings",
+    },
+    {
+      icon: <RiLogoutBoxRLine />,
+      paths: ["/"],
+    },
+  ];
 
-]
+  return (
+    <React.Fragment>
+      <Grid>
+        <Navbar />
 
-    return (
+        <Grid container className={classes.grid}>
+          <Grid container className={classes.sidebar}>
+            <Paper className={classes.sidebarPaper}>
+              <Grid container className={classes.icons}>
+                {icons.map((i, k) => {
+                  return i.paths.includes(history.location.pathname) ? (
+                    <IconButton
+                      onClick={() => history.push(i.path)}
+                      className={[classes.activeIconBtn, classes.iconBtn]}
+                    >
+                      {i.icon}
+                    </IconButton>
+                  ) : (
+                    <IconButton
+                      onClick={() => history.push(i.paths[0])}
+                      key={k}
+                      className={classes.iconBtn}
+                    >
+                      {i.icon}
+                    </IconButton>
+                  );
+                })}
+              </Grid>
+            </Paper>
+          </Grid>
 
-<React.Fragment>
+          <Switch>
+            <Route exact path="/dashboard">
+              <Grid style={{ margin: "auto" }}>
+                {history.location.pathname.split("/").slice(-1)}
+              </Grid>
+            </Route>
+            <Route path="/dashboard/orders">
+              <Grid style={{ margin: "auto" }}>
+                {history.location.pathname.split("/").slice(-1)}
+              </Grid>
+            </Route>
 
+            <Route path="/dashboard/product" component={Products} />
 
+            <Route exact path="/dashboard/categories" component={Categories} />
 
+            <Route path="/dashboard/categories/options" component={Options} />
 
-
-
-        <Grid>
-            <Navbar/>
-
-<Grid container className={classes.grid} >
-
-<Grid container className={classes.sidebar}>
-
-<Paper className={classes.sidebarPaper}>
-
-<Grid container className={classes.icons}>
-
-
-
-{
-    icons.map((i,k)=>{
-  return  i.paths.includes(history.location.pathname)  ?(
-<IconButton 
-onClick={()=>history.push(i.path)}
-className={[classes.activeIconBtn,classes.iconBtn]}>
-{i.icon}
-</IconButton>
-    ):(
-    <IconButton
-    onClick={()=>history.push(i.paths[0])}
-    key={k} className={classes.iconBtn} >
-{i.icon}
-    </IconButton>
-    )
-    })
-}
-
-</Grid>
-
-
-
-</Paper>
-
-</Grid>
-
-<Switch>
-
-<Route exact path="/dashboard" >
-<Grid style={{margin: 'auto'}}>
-    {history.location.pathname.split('/').slice(-1)}  
-</Grid>
-</Route>
-<Route path="/dashboard/orders" >
-<Grid style={{margin: 'auto'}}>
-    {history.location.pathname.split('/').slice(-1)}  
-</Grid>
-</Route>
-
-<Route path="/dashboard/product" component={Products} />
-
-<Route exact  path="/dashboard/categories" component={Categories}  />
-
-
-<Route  path="/dashboard/categories/options" component={Options}  />
-
-
-<Route path="/dashboard/customers" >
-<Grid style={{margin: 'auto'}}>
-    {history.location.pathname.split('/').slice(-1)}  
-</Grid>
-</Route>
-<Route path="/dashboard/coupons" >
-<Grid style={{margin: 'auto'}}>
-    {history.location.pathname.split('/').slice(-1)}  
-</Grid>
-</Route>
-<Route path="/dashboard/apps" >
-<Grid style={{margin: 'auto'}}>
-    {history.location.pathname.split('/').slice(-1)}  
-</Grid>
-</Route>
-<Route path="/dashboard/services" >
-<Grid style={{margin: 'auto'}}>
-    {history.location.pathname.split('/').slice(-1)}  
-</Grid>
-</Route>
-<Route path="/dashboard/store" component={Store} />
-<Route path="/dashboard/users" >
-<Grid style={{margin: 'auto'}}>
-    {history.location.pathname.split('/').slice(-1)}  
-</Grid>
-</Route>
-<Route path="/dashboard/settings" component={Settings} />
-
-
-
-
-</Switch>
-
-
-
-</Grid>
-
+            <Route path="/dashboard/customers">
+              <Grid style={{ margin: "auto" }}>
+                {history.location.pathname.split("/").slice(-1)}
+              </Grid>
+            </Route>
+            <Route path="/dashboard/coupons">
+              <Grid style={{ margin: "auto" }}>
+                {history.location.pathname.split("/").slice(-1)}
+              </Grid>
+            </Route>
+            <Route path="/dashboard/apps">
+              <Grid style={{ margin: "auto" }}>
+                {history.location.pathname.split("/").slice(-1)}
+              </Grid>
+            </Route>
+            <Route path="/dashboard/services">
+              <Grid style={{ margin: "auto" }}>
+                {history.location.pathname.split("/").slice(-1)}
+              </Grid>
+            </Route>
+            <Route path="/dashboard/store" component={Store} />
+            <Route path="/dashboard/users">
+              <Grid style={{ margin: "auto" }}>
+                {history.location.pathname.split("/").slice(-1)}
+              </Grid>
+            </Route>
+            <Route path="/dashboard/settings" component={Settings} />
+          </Switch>
         </Grid>
+      </Grid>
+    </React.Fragment>
+  );
+};
 
-
-</React.Fragment>
-)
-}
-
-const styles=theme=>({
-
-grid:{
+const styles = (theme) => ({
+  grid: {
     // height:'121vh',
-    width:'100%',
-    background:theme.palette.primary.dashboardBackground,
-    display:'flex',
-    overflow:'hidden',
-    maxWidth:'100%'
+    width: "100%",
+    background: theme.palette.primary.dashboardBackground,
+    display: "flex",
+    overflow: "hidden",
+    maxWidth: "100%",
     // bottom:0
-},
-sidebar:{
-    minHeight:'90vh',
-    width:'4rem'
-},
-sidebarPaper:{
+  },
+  sidebar: {
+    minHeight: "90vh",
+    width: "4rem",
+  },
+  sidebarPaper: {
     // height:'131vh',
-    width:'100%',
-    paddingTop:'5vh'
-},
-icons:{
-    minHeight:'78vh',
-    display:'flex',
-    flexDirection:'column',
-    alignItems:'center',
-    justifyContent:'space-around'
-},
-iconBtn:{
-    width:'100%',
-    borderRadius:'0px',
-    color:'#babbbf',
-    '&:hover':{
-        color:'#52565e',
-    }
-},
-activeIconBtn:{
-color:'#52565e',
-background:theme.palette.primary.iconBtnBackground,
-borderLeft:`5px solid ${theme.palette.primary.main}`
-}
+    width: "100%",
+    paddingTop: "5vh",
+  },
+  icons: {
+    minHeight: "78vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  iconBtn: {
+    width: "100%",
+    borderRadius: "0px",
+    color: "#babbbf",
+    "&:hover": {
+      color: "#52565e",
+    },
+  },
+  activeIconBtn: {
+    color: "#52565e",
+    background: theme.palette.primary.iconBtnBackground,
+    borderLeft: `5px solid ${theme.palette.primary.main}`,
+  },
+});
 
-})
-
-export default withRouter(withStyles(styles)(Dashboard))
+export default withRouter(withStyles(styles)(Dashboard));
